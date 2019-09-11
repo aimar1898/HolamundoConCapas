2ec469bcbc4d68aeda737b7a281e324e81a9cd38
fanfu <fanfu@DESKTOP-K965IRE>
2019-09-11 14:27:27 +0200

    Creamos el proyecto y la estructura de los paquetes, clases e interfaces. Desarrollamos las interfaces.

---
+++ src/holamundoconcapas/model/ModelImplementation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoconcapas.model;
+
+/**
+ *
+ * @author fanfu
+ */
+public class ModelImplementation {
+    
+    public String getGreeting() {
+        return "Hello World!";
+    }
+}
---
+++ src/holamundoconcapas/model/ModelInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoconcapas.model;
+
+/**
+ *
+ * @author fanfu
+ */
+public interface ModelInterface {
+    
+    public String getGreeting();
+}
