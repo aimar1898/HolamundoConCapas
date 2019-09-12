ddd91f5464f103d3ee4bfa2652acf4c61a79edd6
fanfu <fanfu@DESKTOP-K965IRE>
2019-09-12 09:36:49 +0200

    Creamos la clase de prueba de ModelFactory y creamos la rama en github.

---
+++ test/holamundoconcapas/model/ModelFactoryTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoconcapas.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fanfu
+ */
+public class ModelFactoryTest {
+    
+    public ModelFactoryTest() {
+    }
+
+    /**
+     * Test of getModel method, of class ModelFactory.
+     */
+    @Test
+    public void testGetModel() {
+    }
+    
+}
